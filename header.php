9a7eb6d5cc8f2e136fcd7a0f47e41f349f735859
xxxxxChristian02xxxxx <negcris02@gmail.com>
2023-11-09 22:55:50 +0100

    <header.php> aggiunto controllo sull admin : se ssono un admin ho accesso anche al "editusers"

--- header.php
+++ header.php
@@ -1,8 +1,44 @@
 <?php
-if(isset($_SESSION['loggedIn'])){
-    echo "<a href=main.php>| Home </a>";
-    echo "<a href=''>| Show profile </a>";
-    echo "<a href=logout.php>| Logout | </a>";
+if(isset($_SESSION['loggedIn'])) {
+
+    //creo la connesione al DB
+    require("connection.php");
+
+    //Query per ottenere il privilegio dell'user
+    $email = $con->real_escape_string($email);
+
+    //Creazione del prepared statement
+    $role_stmt = $con->prepare("SELECT ROLES FROM USERS WHERE MAIL =?");
+    $role_stmt->bind_param('s', $email);
+
+    // Sposto risultato della query in una variabile
+    $role_stmt->bind_result($roles);
+    $role_stmt->fetch();
+
+    //Esecuzione della query
+    $role_stmt->execute();
+
+    //controllo che la query sia andata a buon fine
+    if ($role_stmt->num_rows() === 0){
+        echo "no rows inserted / updated / canceled";
+    }
+
+    // Chiusura del prepared statement
+    $role_stmt->close();
+
+    //ciusura connessione
+    $con->close();
+
+    if($roles == 1){
+        echo "<a href=main.php>| Home </a>";
+        echo "<a href=''>| Show profile </a>";
+        echo "<a href=logout.php>| Logout | </a>";
+        echo "<a href= editusers.php>| Edit Users |</a>";
+    }else {
+        echo "<a href=main.php>| Home </a>";
+        echo "<a href=''>| Show profile </a>";
+        echo "<a href=logout.php>| Logout | </a>";
+    }
 }
 else{
     echo "<a href=index.php>| Home </a>";
