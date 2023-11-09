9ff02b63c1f9e35e394a4ad5e29dccaf1ea8d142
xxxxxChristian02xxxxx <negcris02@gmail.com>
2023-11-09 22:25:52 +0100

    <connection.php> modificato la connesione aggiunto die al posto di echo

--- connection.php
+++ connection.php
@@ -2,7 +2,7 @@
 $con = new mysqli("localhost", "root", "", "s5223956");
 
 if ($con->connect_errno) {
-    echo "Failed to connect to MySQL: " . $con -> connect_error;
+    die("Failed to connect to MySQL: " . $con -> connect_error);
     exit();
 }
 ?>
