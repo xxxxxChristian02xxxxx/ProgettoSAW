c86f7bfe82018046eecb654bc9126a5ac2a1d4ed
xxxxxChristian02xxxxx <negcris02@gmail.com>
2023-11-10 18:36:53 +0100

    <header.php> agguunta componente per gestire il cookie del remember me

--- header.php
+++ header.php
@@ -1,13 +1,19 @@
 <?php
 if(isset($_SESSION['loggedIn'])) {
 
+    $email = $_POST['email'];
+    $email = trim($email);
+    $password = $_POST['pass'];
+    $password = trim($password);
+    require('session.php');
+
     //creo la connesione al DB
     require("connection.php");
 
-    //Query per ottenere il privilegio dell'user
+    //Sanificazione input
     $email = $con->real_escape_string($email);
 
-    //Creazione del prepared statement
+    //Creazione del prepared statement per ottenere il privilegio dell'user
     $role_stmt = $con->prepare("SELECT ROLES FROM USERS WHERE MAIL =?");
     $role_stmt->bind_param('s', $email);
 
@@ -15,9 +21,11 @@ if(isset($_SESSION['loggedIn'])) {
     $role_stmt->execute();
     $role_stmt->bind_result($roles);
     $role_stmt->fetch();
+    echo $role_stmt." ".$roles;
+
 
     //controllo che la query sia andata a buon fine
-    if ($role_stmt->num_rows === 0){
+    if ($role_stmt->affected_rows == 0){
         echo "no rows inserted / updated / canceled";
     }
 
@@ -37,6 +45,42 @@ if(isset($_SESSION['loggedIn'])) {
         echo "<a href=''>| Show profile </a>";
         echo "<a href=logout.php>| Logout | </a>";
     }
+
+    //verifica che il cookie sia statto messo
+    if(isset($_COOKIE['ReMe'])) {
+        //se il cookie continee un :
+        if(strstr($cookie,":")) {
+            $cookie = $_COOKIE['ReMe'];
+            $parts = explde(":", $cookie);
+            $token_key = $parts[0];
+            $token_value = $parts[1];
+
+            $stm = $con->prepare("SELECT * FROM USERS WHERE TOKEN_KEY =?");
+            $stm->bind_param('s', $token_key);
+
+            //Esecuzione della query
+            $stm->execute();
+            $stm->bind_result($rows);
+            $stm->fetch();
+
+
+            if($con->affected_rows == 0){
+                //Utente registrtato correttamente, posso chiudere la connessione
+                $con->close();
+                echo "non  a buon fine";
+
+            }
+            //non se sia giusto
+            $row = $rows[0];
+            if($token_value ==$rows['token_value']){
+                $_SESSION['loggedIn'] = $row;
+            }
+
+        }
+    }
+
+
+
 }
 else{
     echo "<a href=index.php>| Home </a>";
