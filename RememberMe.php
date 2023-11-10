5ec302f8302d500421928a7482791973992b0cc7
xxxxxChristian02xxxxx <negcris02@gmail.com>
2023-11-10 18:34:30 +0100

    <rememberme.php> fatto un controllo sulla spunta remember del form ,aggiunto la creazione del cookie che contreea i valori della chiave valore dell'utente che dovranno essere poi salvai nella tabella USERS (usando metodo per criptare i dati)

---
+++ RememberMe.php
@@ -0,0 +1,35 @@
+<?php
+session_start();
+
+        require ('session.php');
+        //set cookie se il rememberme è settato su true
+        if ($remember) {
+
+            $expires = time() + ((60 * 60 * 24) * 7);
+            $salt = "%salt&/";
+            //creo un token key random per non risalire alle credenziali dell'utente dal cookie
+            //per rendere la proteine del cookie piu forte ci metto il sale
+
+            //faccio hash cosicchè la chiave e il valore del mio cookie, essendo che contengono dati sensibili, vengano encriptati in modo pseudorandomico
+            $token_key = hash('sha256', (time() . $salt));
+            $token_value = hash('sha256', ("logged_in" . $salt));
+            setcookie('ReMe', $token_key . ':' . $token_value, $expires, '/');
+            require("connection.php");
+
+            // Preparazione della query con prepared statement
+            $query = "UPDATE USERS SET token_key=?, token_value=? WHERE ID=?";
+            $stmt = $con->prepare($query);
+            $stmt->bind_param('ssi', $token_key, $token_value, $id);
+
+            // Assegnazione dei valori ai parametri e esecuzione della query
+            $token_key = $row['token_key'];
+            $token_value = $row['token_value'];
+            $id = $row['id'];
+            $stmt->execute();
+
+
+
+        }
+
+?>
+
