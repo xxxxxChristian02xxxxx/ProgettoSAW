5faa9aa951ff6535eb07a84ae2cda1f6b6cc5b69
xxxxxChristian02xxxxx <negcris02@gmail.com>
2023-11-10 17:02:46 +0100

    <logout.css> messo una if

--- logout.php
+++ logout.php
@@ -1,8 +1,10 @@
 <?php
 session_start();
+if(isset($_SESSION["loggedIn"])) {
+    session_destroy();
 
-session_destroy();
+    header("Location: index.php");
+    exit();
 
-header("Location: index.php");
-exit();
+}
 ?>
