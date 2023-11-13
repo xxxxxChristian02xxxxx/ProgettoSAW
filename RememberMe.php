c7b1243891e7cab0ee6b4aaf2717a32e4d44eae3
edoardomeloni <edomelo02@gmail.com>
2023-11-13 16:13:28 +0100

    Implementazione parziale RememberMe

--- RememberMe.php
+++ RememberMe.php
@@ -1,36 +1,32 @@
 <?php
-session_start();
+    require ('session.php');
+    //set cookie se il rememberme è settato su true
+    if ($remember) {
+        $expire = time() + ((60 * 60 * 24) * 7);
+        $salt = "%salt&/";
+        //creo un token key random per non risalire alle credenziali dell'utente dal cookie
+        //per rendere la proteine del cookie piu forte ci metto il sale
 
-        require ('session.php');
-        //set cookie se il rememberme è settato su true
-        if ($remember) {
+        //faccio hash cosicchè la chiave e il valore del mio cookie, essendo che contengono dati sensibili, vengano encriptati in modo pseudorandomico
+        $token_key = hash('sha256', (time() . $salt));
+        $token_value = hash('sha256', ("logged_in" . $salt));
 
-            $expires = time() + ((60 * 60 * 24) * 7);
-            $salt = "%salt&/";
-            //creo un token key random per non risalire alle credenziali dell'utente dal cookie
-            //per rendere la proteine del cookie piu forte ci metto il sale
+        $sessiondata = json_encode(['token_value' => $token_value, 'data' => $_SESSION]);
+        setcookie('ReMe', $sessiondata, $expire, '/');
+        $expire = date("Y-m-d", $expire);
+        require("connection.php");
 
-            //faccio hash cosicchè la chiave e il valore del mio cookie, essendo che contengono dati sensibili, vengano encriptati in modo pseudorandomico
-            $token_key = hash('sha256', (time() . $salt));
-            $token_value = hash('sha256', ("logged_in" . $salt));
-            setcookie('ReMe', $token_key . ':' . $token_value, $expires, '/');
-            require("connection.php");
+        // Preparazione della query con prepared statement
+        $query = "UPDATE USERS SET TOKEN=?, EXPIRE=? WHERE ID=?";
+        $stmt = $con->prepare($query);
+        $stmt->bind_param('ssi', $token_value, $expire, $id);
 
-            // Preparazione della query con prepared statement
-            $query = "UPDATE USERS SET token_key=?, token_value=? WHERE ID=?";
-            $stmt = $con->prepare($query);
-            $stmt->bind_param('ssi', $token_key, $token_value, $id);
+        // Assegnazione dei valori ai parametri e esecuzione della query
+        //$token_key = $row['token_key'];
+        //$token_value = $row['token_value'];
+        //$id = $row['id'];
+        $stmt->execute();
 
-            // Assegnazione dei valori ai parametri e esecuzione della query
-            $token_key = $row['token_key'];
-            $token_value = $row['token_value'];
-            $id = $row['id'];
-            $stmt->execute();
-
-
-
-
-        }
-
+    }
 ?>
 
