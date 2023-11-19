ac7fe4266262ab58fb32f7a2a1263efdce335d4f
xxxxxChristian02xxxxx <negcris02@gmail.com>
2023-11-19 19:28:55 +0100

    prova

--- connection.php
+++ connection.php
@@ -2,7 +2,7 @@
 $con = new mysqli("localhost", "s5223956", "Dadedi1917", "s5223956");
 
 if ($con->connect_errno) {
-    die("Failed to connect to server MySQL: " . $con -> connect_error);
+    die("Failed to connecteddd to server MySQL: " . $con -> connect_error);
 
 }
 ?>
