a07111ec9ba791a6216175e6547e535661851c58
xxxxxChristian02xxxxx <negcris02@gmail.com>
2024-02-01 14:56:26 +0100

    <main.php> aggiunto swtch tra timer e cronometro, messo

--- js/main_timer.js
+++ js/main_timer.js
@@ -1,5 +1,54 @@
+//-------------------------FUNZIONE PER POPOLARE IL MENU A TENDINA   -------------------------//
+function populateSelect(options) {
+    subChoosen.innerHTML = "";
+    subChoosen.innerHTML = "<div><option value=''> </option> <span> <button> - </button ></span></div>";
+    options.forEach(option => {
+        var optionElement = document.createElement("option");
+        optionElement.value = option;
+        optionElement.textContent = option;
+        subChoosen.appendChild(optionElement);
+    });
 
-//-------------------------FUNZIONE PER IL TOGGLE -------------------------//
+}
+//-------------------------FUNZIONE VEDERE SE LA MATERIA AGGIUNTA è VALIDA    -------------------------//
+function isSubPresent(addSubject){
+    console.log(displaySubjects);
+    // esempio :
+    // sub.replace(/\s/g, '').toLowerCase() =>tolgo gli spazi e metto no case sensitive
+    for (let i = 0; i < displaySubjects.length; i++) {
+        var subpr = displaySubjects[i].replace(/\s/g, '').toLowerCase();
+        var subnew = addSubject.toString().replace(/\s/g, '').toLowerCase();
+        console.log(subnew, ",", subpr);
+        if (subnew === subpr) {
+            console.log("sono qui");
+            return false;
+        }
+    }
+    return true;
+
+
+}
+
+function blockSelection(){
+
+}
+function unlockSelection(){
+
+}
+//-------------------------FUNZIONE PER IL PER TIMER E STOPWATCH -------------------------//
+function toggleButtonTS(buttonId) {
+    if ((swipeCount % 2 )=== 0) {
+        displayTimer.classList.remove("hide");
+        displayStopwatch.classList.add("hide");
+        idTimerOrStopwatch = true;
+    } else {
+        displayStopwatch.classList.remove("hide");
+        displayTimer.classList.add("hide");
+        idTimerOrStopwatch=false;
+    }
+}
+
+//-------------------------FUNZIONE PER IL TOGGLE TASTO START -------------------------//
 function toggleButton(buttonId){
     // Riferimento all'elemento del bottone
     var button = document.getElementById(buttonId);
@@ -15,27 +64,30 @@ function toggleButton(buttonId){
     console.log(counting);
 }
 //-------------------------FUNZIONE PER IL TOGGLE -------------------------//
-    function showStudySession(){
-        moneyMoneyObtained.innerHTML =timeSpentForMoney;
-        timeDuratioSession.innerHTML=  formattedTime;
-        subSubStudied.innerHTML = null;
+function showStudySession(){
+    console.log(subSubStudied);
+    timeDuratioSession.innerHTML=  timmeSpentForSession;
+    moneyMoneyObtained.innerHTML =timeSpentForMoney;
+    subSubStudied.innerHTML = subEventuallyStudied.value;
 
-    }
-    //-------------------------FUNZIONE PER FARE L'UPDATE DEL TIMER -------------------------//
-    function updateTimer() {
-        let minutes = Math.floor(timeLeft / 60);
-        let seconds = timeLeft % 60;
-        /*  funzione per stampare a schermo il tempo che scorre ,padStart serve per stampare 0, col lo 0 davanti con 2 -> voglio 2 digit e se non ho nulla metto "0" di default*/
-        formattedTime = `${minutes.toString().padStart(2, "0")} : ${seconds.toString().padStart(2, "0")}`;
-        timeElement.innerHTML = formattedTime;
+}
+//-------------------------FUNZIONE PER FARE L'UPDATE DEL TIMER -------------------------//
+function updateTimer() {
 
+    let minutes = Math.floor(timeLeft / 60);
+    let seconds = timeLeft % 60;
+    /*  funzione per stampare a schermo il tempo che scorre ,padStart serve per stampare 0, col lo 0 davanti con 2 -> voglio 2 digit e se non ho nulla metto "0" di default*/
+    formattedTime = `${minutes.toString().padStart(2, "0")} : ${seconds.toString().padStart(2, "0")}`;
+    timeElement.innerHTML = formattedTime;
+    timmeSpentForSession ++;
 
-    }
-    //-------------------------FUNZIONE PER IL INZIARE E STOPPARE IL TIMER IL TIMER -------------------------//
-    function startTimer() {
-        /*intervallo che deve essere aggiornato ogni 1000 ms*/
-        toggleButton('Timer');
-        interval = setInterval(() => {
+}
+//-------------------------FUNZIONE PER IL INZIARE E STOPPARE IL TIMER IL TIMER -------------------------//
+function startTimer() {
+
+    /*intervallo che deve essere aggiornato ogni 1000 ms*/
+    toggleButton('Timer');
+    interval = setInterval(() => {
         timeLeft--;
         timeSpentForMoney++
         updateTimer();
@@ -43,50 +95,62 @@ function toggleButton(buttonId){
             /*una vlta finito il timer pulisco l'intervallo*/
             clearInterval(interval);
             alert("finito");
-            timeLeft = 1500;
+            timeLeft = 1500 ;
             updateTimer();
         }
-        }, 1000)
-    }
-    //-------------------------FUNZIONE PER IL RESETTARE IL TIMER -------------------------//
-    function resetTimer() {
-        console.log("resetTimer")
-        clearInterval(interval);
+    }, 1000)
+}
+//-------------------------FUNZIONE PER IL RESETTARE IL TIMER -------------------------//
+function resetTimer() {
+    console.log("resetTimer")
+    clearInterval(interval);
 
-        timeLeft = 1500;
-        showStudySession();
-        updateTimer();
-        databaseDelivery(dataTime);
+    timeLeft = 1500;
+    showStudySession();
+    updateTimer();
+    console.log("mando al back");
+    const dataTime={
+        typeSession:idTimerOrStopwatch,
+        timeSpent :timmeSpentForSession,
+        money : timeSpentForMoney,
+        subjactName: subEventuallyStudied.value,
+        description:null,
+        season:null
+    }
+    console.log("json: ",dataTime);
 
-    }
-    //-------------------------FUNZIONE PER FERMARE IL TIMER  -------------------------//
-    function stopTimer() {
-        clearInterval(interval);
-        toggleButton('Timer');
-        console.log(startTime, formattedTime);
-    }
+    databaseDelivery(dataTime);
+
+}
+//-------------------------FUNZIONE PER FERMARE IL TIMER  -------------------------//
+function stopTimer() {
+    clearInterval(interval);
+    toggleButton('Timer');
+    console.log(startTime, formattedTime);
+}
 
 function databaseDelivery(dataTime){
+    console.log("entratat nella funzione");
 
+    fetch('../backend/main_backend.php', { // dico il percorso del file di back end
+        method: 'POST', //metodo get o post
+        headers: {
+            'Content-Type': 'application/json' // specifico la uso
+        },
+        body: JSON.stringify(dataTime) // encode
+    })
+        .then(response => response.json()) //prendo la risposta di login backend(ha ottenuto i risultati delle query ) e li ha messi nella variabile
 
-        fetch('../backend/main_backend.php', { // dico il percorso del file di back end
-            method: 'POST', //metodo get o post
-            headers: {
-                'Content-Type': 'application/json' // specifico la uso
-            },
-            body: JSON.stringify(dataTime) // encode
+        .then(data => { //prendo i dati ottenuti e li processo
+
+            if (data.success) {
+                window.location.href = '../main.php'; // se chiamata è andata bene faccio display del main.php
+            } else {
+                window.alert('something has failed'); //altimenti mando messaggio di errore
+            }
         })
-            .then(response => response.json()) //prendo la risposta di login backend(ha ottenuto i risultati delle query ) e li ha messi nella variabile
-            .then(data => { //prendo i dati ottenuti e li processo
-
-                if (data.success) {
-                    window.location.href = '../main.php'; // se chiamata è andata bene faccio display del main.php
-                } else {
-                    window.alert('something has failed'); //altimenti mando messaggio di errore
-                }
-            })
-            .catch(error => {
-                console.error('Error:', error);
-            });
+        .catch(error => {
+            console.error('Error:', error);
+        });
 
 }
