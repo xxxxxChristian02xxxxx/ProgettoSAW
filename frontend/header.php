d1ab1a6b1ad57ed3be76cf2d16839294688a44ee
pesimachete <ph.simao.achete@gmail.com>
2024-02-06 15:53:11 +0100

    rosolving warning file

--- frontend/header.php
+++ frontend/header.php
@@ -1,9 +1,9 @@
 <?php
 echo "<link href='../dressing.css' rel='stylesheet' type='text/css'>";
 if(isset($_SESSION['loggedIn'])) {
-    include('backend/function_files/session.php');
+    include('../backend/function_files/session.php');
     $session = getSession(true);
-    include('private_header.html');
+    include('private_header.php');
 }
 else{
     include('public_header.html');
