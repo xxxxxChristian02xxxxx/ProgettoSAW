191a8bf7ab87bc7d8ca6973035d4ab5821f9db37
edoardomeloni <edomelo02@gmail.com>
2024-02-09 20:56:03 +0100

    Aggiustamento edituser (quando si banna si modifica subito la tabella)

--- js/editusers.js
+++ js/editusers.js
@@ -52,10 +52,11 @@ function banUnban() {
                     body: JSON.stringify({action: 'banUnban', email: email.innerText})
                 })
                     .then(response => {
-                        console.log('okkk');
+                        return response.json();
                     })
                     .then(data => {
                         console.log(data);
+                        cell.innerText = data;
                     })
                     .catch(error => {
                         console.error("qualcosa è andato storto");
