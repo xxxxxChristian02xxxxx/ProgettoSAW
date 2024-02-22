899e8d424ad8d8b1bae9616754c59bd414b0e6af
edoardomeloni <edomelo02@gmail.com>
2024-02-22 22:41:11 +0100

    - aggiustamento query study sessions - controllo java login

--- js/login.js
+++ js/login.js
@@ -7,38 +7,47 @@ document.getElementById('UserLogin').addEventListener('submit', function (event)
         pass: document.getElementById('pass').value,
         ReMe: document.getElementById('ReMe').checked
     };
-
-    //chimata dal frontend login in backend login
-    fetch('../backend/be_login.php', { // dico il percorso del file di back end
-        method: 'POST', //metodo get o post
-        headers: {
-            'Content-Type': 'application/json' // specifico la uso
-        },
-        body: JSON.stringify(formData) // encode
-    })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            if (response.status === 204) { // No content
-                return null;
-            }
-            return response.json();
-        }) //prendo la risposta di login backend(ha ottenuto i risultati delle query ) e li ha messi nella variabile
-        .then(data => { //prendo i dati ottenuti e li processo
-
-            if (data.success && !data.banned) {
-                window.location.href = 'main.php'; // se chiamata è andata bene faccio display del main.php
-            }
-            else if(!data.success && data.banned) {
-                window.alert("You can't login, you're banned!");
-            }
-            else {
-                window.alert("Login failed"); //altimenti mando messaggio di errore
-            }
+    if(formData['email'] && formData['pass']){
+        //chimata dal frontend login in backend login
+        fetch('../backend/be_login.php', { // dico il percorso del file di back end
+            method: 'POST', //metodo get o post
+            headers: {
+                'Content-Type': 'application/json' // specifico la uso
+            },
+            body: JSON.stringify(formData) // encode
         })
-        .catch(error => {
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                if (response.status === 204) { // No content
+                    return null;
+                }
+                return response.json();
+            }) //prendo la risposta di login backend(ha ottenuto i risultati delle query ) e li ha messi nella variabile
+            .then(data => { //prendo i dati ottenuti e li processo
 
-            console.error('Error:', error);
-        });
+                if (data.success && !data.banned) {
+                    window.location.href = 'main.php'; // se chiamata è andata bene faccio display del main.php
+                }
+                else if(!data.success && data.banned) {
+                    window.alert("You can't login, you're banned!");
+                }
+                else {
+                    window.alert("Login failed"); //altimenti mando messaggio di errore
+                }
+            })
+            .catch(error => {
+
+                console.error('Error:', error);
+            });
+    }
+    else{
+        if(!formData['email']){
+            alert("Insert an email to login");
+        }
+        else{
+            alert("Insert password to login");
+        }
+    }
 });
